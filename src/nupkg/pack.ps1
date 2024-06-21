724258a46aa3adb0d472ca80b909b6df4bbda6b5
leafkevin <leafkevin@outlook.com>
2024-06-21 13:02:49 +0800

    更新sql生成T4模板

--- src/nupkg/pack.ps1
+++ src/nupkg/pack.ps1
@@ -3,7 +3,16 @@ cd $PSScriptRoot
 
 # 定义所有项目
 $projects = (
-  "Thea.Job"
+	"Thea",
+	"Thea.Alarm",
+	"Thea.JwtToken",
+	"Thea.Logging",
+	"Thea.Logging.Alarm",
+	"Thea.Logging.Template",
+	"Thea.Logging.Alarm",
+	"Thea.MessageDriven",
+	"Thea.Job",
+	"Thea.Web"
 )
 Remove-Item *.nupkg -recurse
 cd ..
@@ -13,7 +22,7 @@ foreach($project in $projects) {
     cd $project
 	dotnet clean
 	dotnet build -c Release
-    dotnet pack -c Release
+    dotnet pack -c Release -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg
     mv ./bin/Release/*.nupkg ../nupkg
     cd ..
 }
