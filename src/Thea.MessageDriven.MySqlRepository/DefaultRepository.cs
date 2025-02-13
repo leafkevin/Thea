a043fa0647d2e617549c3549382f37a86af92ac7
leafkevin <leafkevin@outlook.com>
2025-02-13 13:19:56 +0800

    消息驱动仓储增加redis

--- src/Thea.MessageDriven.MySqlRepository/DefaultRepository.cs
+++ src/Thea.MessageDriven.MySqlRepository/DefaultRepository.cs
@@ -12,21 +12,38 @@ public class DefaultRepository : IMessageDrivenRepository
 {
     private readonly string dbKey;
     private readonly IOrmDbFactory dbFactory;
+    private readonly IDistributedCache redisCache;
     public DefaultRepository(IServiceProvider serviceProvider)
     {
         this.dbFactory = serviceProvider.GetService<IOrmDbFactory>();
         var configuration = serviceProvider.GetService<IConfiguration>();
         this.dbKey = configuration.GetValue<string>("MessageDriven:DbKey");
+        this.redisCache = serviceProvider.GetService<IDistributedCache>();
     }
     public virtual async Task<List<Cluster>> GetClusters(List<string> clusterIds)
     {
-        var repository = this.dbFactory.Create(this.dbKey);
-        return await repository.QueryAsync<Cluster>(f => clusterIds.Contains(f.ClusterId));
+        var result = new List<Cluster>();
+        foreach (var clusterId in clusterIds)
+        {
+            var cacheKey = $"mds.cluster.{clusterId}";
+            var myCluster = await this.redisCache.GetOrCreateAsync(cacheKey, async () =>
+            {
+                var repository = this.dbFactory.Create(this.dbKey);
+                return await repository.QueryFirstAsync<Cluster>(f => clusterIds.Contains(f.ClusterId));
+            });
+            result.Add(myCluster);
+        }
+        return result;
     }
     public virtual async Task Register(List<Cluster> clusters)
     {
         var repository = this.dbFactory.CreateRepository(this.dbKey);
         await repository.CreateAsync<Cluster>(clusters);
+        foreach (var cluster in clusters)
+        {
+            var cacheKey = $"mds.cluster.{cluster.ClusterId}";
+            await this.redisCache.SetAsync(cacheKey, cluster);
+        }
     }
     public virtual async Task WriteLogs(List<ExecLog> logInfos)
     {
