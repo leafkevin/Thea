5fd12fd38e6138a400cb8fe82b250757a215a08d
leafkevin <leafkevin@outlook.com>
2025-03-12 16:09:13 +0800

    update

--- src/Thea.Auth/TheaJwtTokenService.cs
+++ src/Thea.Auth/TheaJwtTokenService.cs
@@ -1,12 +1,12 @@
-﻿using System;
+﻿using Duende.IdentityModel;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Security.Claims;
 using System.Security.Cryptography;
-using Duende.IdentityModel;
-using Microsoft.Extensions.Options;
-using Microsoft.IdentityModel.Tokens;
 
 namespace Thea.Auth;
 
