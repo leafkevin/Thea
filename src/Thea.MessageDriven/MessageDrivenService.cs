a23c927dd1741091b531abd0c301a8efa36b8594
leafkevin <leafkevin@outlook.com>
2025-07-26 13:25:59 +0800

    update

--- src/Thea.MessageDriven/MessageDrivenService.cs
+++ src/Thea.MessageDriven/MessageDrivenService.cs
@@ -623,10 +623,7 @@ class MessageDrivenService : IMessageDriven
         Type = MessageType.Logs,
         Body = logInfo
     });
-    internal void TransferMessage(Message message)
-    {
-        this.messageQueue.Enqueue(message);
-    }
+    internal void TransferMessage(Message message) => this.messageQueue.Enqueue(message);
     internal void SetRpcResult(string messageId, Message<string> result)
     {
         if (this.rpcWaiters.TryRemove(messageId, out var rpcWaiter))
