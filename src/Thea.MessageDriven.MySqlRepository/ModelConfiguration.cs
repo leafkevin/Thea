81aabac5797b5c9f2693f071c27c52437ea77b1d
leafkevin <leafkevin@outlook.com>
2026-08-13 17:53:19 +0800

    update

--- src/Thea.MessageDriven.MySqlRepository/ModelConfiguration.cs
+++ src/Thea.MessageDriven.MySqlRepository/ModelConfiguration.cs
@@ -6,7 +6,9 @@ class ModelConfiguration : IModelConfiguration
 {
     public void OnModelCreating(ModelBuilder builder)
     {
-        builder.Entity<Setting>(f => f.ToTable("mds_setting"))
+        builder
+            .Entity<Binding>(f => f.ToTable("mds_binding"))
+            .Entity<Queue>(f => f.ToTable("mds_queue"))
             .Entity<ExecLog>(f => f.ToTable("mds_log"));
     }
 }
