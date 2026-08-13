055da72c02d1dfd53276be1d5470ab0f31fb5448
leafkevin <leafkevin@outlook.com>
2026-08-13 17:57:53 +0800

    更新数据库脚本

--- src/Thea.MessageDriven.MySqlRepository/mds.sql
+++ src/Thea.MessageDriven.MySqlRepository/mds.sql
@@ -1,24 +1,34 @@
--- -------------- TABLE [mds_setting] BEGIN----------------
--- DROP TABLE IF EXISTS `mds_setting`;
-CREATE TABLE `mds_setting`
+-- -------------- TABLE [mds_binding] BEGIN----------------
+-- DROP TABLE IF EXISTS `mds_binding`;
+CREATE TABLE `mds_binding`
 (
-    `queue` VARCHAR(50) NOT NULL COMMENT '队列名称',
-    `exchanges` VARCHAR(300) NULL COMMENT '交换机',
+    `exchange_id` VARCHAR(50) NOT NULL COMMENT '信箱ID',
+    `queue_id` VARCHAR(50) NOT NULL COMMENT '队列ID',
     `bind_type` VARCHAR(50) NULL COMMENT '绑定类型',
     `binding_key` VARCHAR(50) NULL COMMENT '绑定KEY',
+    `is_delay` TINYINT(1) NULL DEFAULT 0 COMMENT '是否延时消费者'
+    CONSTRAINT `pk_mds_binding` PRIMARY KEY(`exchange_id`,`queue_id`)
+);
+ALTER TABLE `mds_binding` COMMENT '绑定表，描述交换机与队列的绑定关系';
+-- -------------- TABLE [mds_binding] END----------------
+
+
+
+-- -------------- TABLE [mds_queue] BEGIN----------------
+-- DROP TABLE IF EXISTS `mds_queue`;
+CREATE TABLE `mds_queue`
+(
+    `queue_id` VARCHAR(50) NOT NULL COMMENT '队列ID',
+    `app_id` VARCHAR(50) NULL COMMENT '应用ID',
     `is_stateful` TINYINT(1) NULL DEFAULT 0 COMMENT '是否有状态',
-    `workload_total` INT NULL COMMENT '工作负荷个数',
-    `prefetch_count` INT NULL DEFAULT 250 COMMENT '预取个数',
+    `workload_total` INTEGER NULL COMMENT '工作负荷个数',
+    `prefetch_count` INTEGER NULL COMMENT '预取个数',
     `is_log_enabled` TINYINT(1) NULL DEFAULT 0 COMMENT '是否开启日志',
     `is_enabled` TINYINT(1) NOT NULL DEFAULT 1 COMMENT '是否启用',
-    `created_by` VARCHAR(50) NULL COMMENT '创建人',
-    `created_at` DATETIME NULL DEFAULT NOW() COMMENT '创建日期',
-    `updated_by` VARCHAR(50) NULL COMMENT '最后更新人',
-    `updated_at` DATETIME NULL DEFAULT NOW() COMMENT '最后更新日期',
-    CONSTRAINT `pk_mds_setting` PRIMARY KEY(`queue`)
+    CONSTRAINT `pk_mds_queue` PRIMARY KEY(`queue_id`)
 );
-ALTER TABLE `mds_setting` COMMENT '配置表，描述所有的队列、交换机绑定等信息';
--- -------------- TABLE [mds_setting] END----------------
+ALTER TABLE `mds_queue` COMMENT '队列表，描述所有的队列基本信息';
+-- -------------- TABLE [mds_queue] END----------------
 
 
 
