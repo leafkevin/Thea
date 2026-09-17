00f6dd6f4246e96cc68327d467c3ee2de85d0e08
leafkevin <leafkevin@outlook.com>
2026-09-17 19:46:37 +0800

    update

---
+++ src/Thea.Alarm/MessageDrivenAlarmService.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Threading.Tasks;
+using Thea.MessageDriven;
+
+namespace Thea.Alarm;
+
+public class MessageDrivenAlarmService : IAlarmService
+{
+    private readonly IMessageDriven messageDriven;
+    private readonly bool isEnabled;
+    public MessageDrivenAlarmService(IMessageDriven messageDriven, IConfiguration configuration)
+    {
+        this.isEnabled = configuration.GetValue("Alarm:IsEnabled", true);
+        this.messageDriven = messageDriven;
+    }
+    public async Task PostAsync(AlarmRequest request)
+    {
+        if (!this.isEnabled) return;
+        await this.messageDriven.PublishAsync("alarm", "#", request);
+    }
+    public string Escape(string message) => message;
+}
